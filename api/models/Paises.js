89ece9ae1a1c8d3b9bf9d93863c3d630b10a06bd
nguidi <neri3004@gmail.com>
2014-04-10 21:46:32 -0300

    Prueba Autocomplete Paises

---
+++ api/models/Paises.js
@@ -0,0 +1,20 @@
+/**
+ * Paises
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	nombre: 'string'
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
