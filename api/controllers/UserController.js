e41f65d5606deaf309e20c407ae3c0e42763b763
nguidi <neri3004@gmail.com>
2014-08-08 18:22:39 -0300

    Agregado del manejo de Login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,7 +5,58 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
+module.exports	=	{
 	
-};
+	login: function(req,res)
+	{
+		//	Cargo el modulo de encriptacion (https://www.npmjs.org/package/bcrypt)
+		var	bcrypt
+		=	require('bcrypt')
+		//	Busco el usuario que tenga el username solicitado
+		User
+			.findOneByUsername(req.body.username)
+				.populate('profile')
+					.exec(
+						function(err,user)
+						{
+							//	Si ocurrio un error envio un mensaje notificandolo
+							if	(err)
+								res.json({message: 'Error en la DB.'},500)
+							//	Si existe el usuario
+							if	(user)
+								//	Comparo las constraseña con la del usuario cifrada
+								bcrypt
+									.compare(
+										req.body.password
+									,	user.password
+									,	function(err,same)
+										{
+											//	Si coinciden remuevo la password del objeto, instancio la session y devuelvo el usuario 
+											if	(same) {
+												req.session.user = user.id
+												delete user.password
+												res.json(user)	
+											}
+											//	Si no coinciden envio un mensaje notificandolo.
+											else
+												res.json({message: 'Contraseña incorrecta.'},400)
+										}
+									)
+							//	Sino existe el usuario envio un mensaje notificando que no existe
+							else
+								res.json({message: 'Nombre de usuario incorrecto.'},404)
+						}
+					)
+	}
 
+,	logout: function(req,res)
+	{
+		if	(req.session.user) {
+			delete req.session
+			res.json({message: 'Deslogueo satisfactório'},200)
+		}
+		else
+			res.json({message: 'No se encontró una sesión activa'},404)
+	}
+
+};
--- api/models/User.js
+++ api/models/User.js
@@ -26,7 +26,43 @@ module.exports	=	{
 	,	//	User hasOne Profile
 		profile:
 		{
-			model:	'Profile'
+			model:		'Profile'
+		}
+
+
+		//	Encripto la password del usuario. Esto ocurre antes de crear el usuario
+	,	beforeCreate: function(attrs, next)
+		{
+			//	Cargo el modulo de encriptacion (https://www.npmjs.org/package/bcrypt)
+			var	bcrypt
+			=	require('bcrypt')
+			//	Genero un salt de ronda 10 (~10 hashes/sec)
+			bcrypt
+				.genSalt(
+					10
+				,	function(err, salt)
+					{
+						//	Si ocurrio un cancelo la creacion del usuario
+						if (err)
+							return next(err)
+						//	Encripto la contraseña usando el salt generado
+						bcrypt
+							.hash(
+								attrs.password
+							,	salt
+							,	function(err, hash)
+								{
+									//	Si ocurrio un error cancelo la creacion del usuario
+									if (err)
+										return next(err)
+									//	Modifico el atributo password por el password encriptado
+									attrs.password = hash
+									//	Continuo con la creacion del codigo
+									next()
+								}
+							)
+					}
+				)
 		}
 	}
 };
