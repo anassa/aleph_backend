f71f0ab015d405cdd8368279cf38df13cf700992
Neri Guidi <neri.guidi@gmail.com>
2016-11-07 18:18:01 -0200

    Frontend-Backend union

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,12 +5,19 @@ const notFound = require('./not-found-handler');
 const logger = require('./logger');
 
 module.exports = function() {
-  // Add your custom middleware here. Remember, that
-  // just like Express the order matters, so error
-  // handling middleware should go last.
-  const app = this;
+	// Add your custom middleware here. Remember, that
+	// just like Express the order matters, so error
+	// handling middleware should go last.
+	const app = this;
 
-  app.use(notFound());
-  app.use(logger(app));
-  app.use(handler());
+	app.get('/', function (req, res) {
+		if (app.get('enviroment') == "prod")
+			res.sendFile(app.get('public')+'/production.html');
+
+		res.sendFile(app.get('public')+'/development.html');
+	});
+
+	app.use(notFound());
+	app.use(logger(app));
+	app.use(handler());
 };
