f290c2a7d813b105c3786e6f6f1be10e7a7bf8fb
Neri Guidi <neri.guidi@gmail.com>
2016-11-16 00:14:10 -0300

    AutoIncrement library - Articulos field validations

--- src/services/articulos/articulos-model.js
+++ src/services/articulos/articulos-model.js
@@ -6,18 +6,44 @@
 // for more of what you can do here.
 
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence');
 const Schema = mongoose.Schema;
 
 const articulosSchema = new Schema(
 	{
-		createdAt:	{ type: Date, 'default': Date.now }
-	,	updatedAt:	{ type: Date, 'default': Date.now }
+		codigo:			{ type: Number, unique : true }
+	,	usuario:
+		{
+			_id:		{ type: Schema.Types.ObjectId }
+		,	username:	{ type: String }
+		,	permisos:	{ type: Array}
+		}
+	,	nombre:			{ type: String, required: true, unique : true }
+	,	descripcion:	{ type: String }
+	,	unidadMedida:
+		{
+			_id:		{ type: Schema.Types.ObjectId }
+		,	nombre:		{ type: String }
+		}
+	,	stock:			{ type: Number, required: true }
+	,	maximo:			{ type: Number, required: true }
+	,	minimo:			{ type: Number, required: true }
+	,	precioVenta:	{ type: Number }
+	,	precioCosto:	{ type: Number }
+	,	descuento:		{ type: Number }
+	,	ajuste:			{ type: Number }
+	,	proveedores:	{ type: Array }
+	//,	alarmaMax:		{ type: Number, required: true }
+	//,	alarmaMin:		{ type: Number, required: true }
+	,	createdAt:		{ type: Date, 'default': Date.now }
+	,	updatedAt:		{ type: Date, 'default': Date.now }
 	}
 ,	{
 		strict:		false
 	}
 );
 
+articulosSchema.plugin(AutoIncrement, {inc_field: 'codigo'});
 const articulosModel = mongoose.model('articulos', articulosSchema);
 
 module.exports = articulosModel;
